676b029e79dc0c5701cadb87e7bd12bab291b19c
Alekslov <Alekslov@users.noreply.github.com>
2020-12-02 12:58:18 +0200

    clock rendering

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { renderSocials } from "./components/socials/renderSocials.js";
 import { socialsData } from './data/socialsData.js';
+import {renderClock } from './components/clock/renderClock.js'
 renderSocials('footer > .row', socialsData);
+renderClock('.clock');
