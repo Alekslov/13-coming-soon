70bc232d4a812c834d47d796c62b9617ede1c024
Alekslov <Alekslov@users.noreply.github.com>
2020-12-02 17:17:05 +0200

    progress bar renedering and validation

---
+++ js/data/progressBarData.js
@@ -0,0 +1,19 @@
+const progressBarData = [
+    {
+        selector: '.column.left',
+        title: 'Web development',
+        value: 90,
+    },
+    {
+        selector: '.column.left',
+        title: 'Web design',
+        value: 80,
+    },
+    {
+        selector: '.column.left',
+        title: 'UX',
+        value: 40,
+    },
+];
+
+export { progressBarData }
