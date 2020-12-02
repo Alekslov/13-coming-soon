a3c1b2d6e3e552c44c7f4a237afae81fb53a3620
Alekslov <Alekslov@users.noreply.github.com>
2020-12-02 21:02:12 +0200

    clock render

---
+++ js/components/clock/countTimeDiff.js
@@ -0,0 +1,31 @@
+function countTimeDiff {
+    // einamieji metai (2020)
+    const date = new Date ();
+    const currentYear = date.getFullYear();
+    // naujakas = einamieji metai + 1 (2020 + 1 = 2021)
+    const newYear = currentYear + 1;
+    // susikonstruojama pilna data: $(naujakas) -01-01 00:00:00
+    const newYearDate = `${newYear}-01-01 00:00:00`;
+    const newYearObject = new Date(newYearDate);
+    const newYearMiliseconds = newYearObject.getTime();
+    // einamasis laikas yyy-mm-dd hh:mm:ss
+    const currentTimeMiliseconds = date.getTime();
+    // suskaiciuojame laiko skirtuma
+    const timeLeft = newYearMiliseconds - currentTimeMiliseconds;
+    let secondsLeft = timeLeft / 1000;
+    // is skirtumo apskaiciuojame likusias dienas, val, minu, sekundes
+    const days = Math.floor(secondsLeft / 60 / 60 / 24);
+    secondsLeft -= days * 60 * 60 * 24;
+    const hours = Math.floor(secondsLeft / 60 / 60);
+    secondsLeft -= hours * 60 * 60;
+    const minutes = Math.floor (secondsLeft / 60) ;
+    const seconds = Math.floor(secondsLeft - minutes * 60);
+    
+    return {
+    days = days < 10 ? '0' + days : days,
+    hours = hours < 10 ? '0' + hours : hours,
+    minutes = minutes < 10 ? '0' + minutes : minutes,
+    seconds = seconds < 10 ? '0' + seconds : seconds,
+    }
+}
+export{countTimeDiff}
