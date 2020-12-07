ac207459fdec72d712fe59d0a2b60f6699abeab1
Alekslov <73888669+Alekslov@users.noreply.github.com>
2020-12-07 12:51:28 +0200

    Update isValidName.js

--- js/components/form-validator/isValidName.js
+++ js/components/form-validator/isValidName.js
@@ -10,7 +10,28 @@ function isValidName(name) {
     if (name.length > maxNameLength) {
         return `Vardas negali virsyti ${maxNameLength} simboliu (virsyta ${name.length - maxNameLength} simboliu).`;
     }
+
+    // turi buti tik abeceles raides
+    const abc = 'qwertyuiopasdfghjklzxcvbnm';
+    for (let letter of name) {
+        if (!abc.includes(letter.toLowerCase())) {
+            return `Varde panaudota neleistinas simbolis (${letter}).`;
+        }
+    }
+
+    // jei gauto vardo raides pavertus mazosiomis yra lygu pradinei reiksmei, tai reiskia, jog vardas ir buvo vien tik is mazuju raidziu
+    if (name.toLowerCase() === name) {
+        return 'Vardas negali buti vien tik is mazuju raidziu';
+    }
+
+    // jei pagauname, jog ivesta vien didziosiomis, tai grazinam zinute - isjunk CapsLock
+
+    const realNameFormat = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    if (realNameFormat !== name) {
+        return 'Varda turi sudaryti pirma didzioji raide ir likusios mazosios raides.';
+    }
+
     return true;
 }
 
-export { isValidName } 
+export { isValidName }
