c6c287a1f9ba411edb9c3ef3b5c9606419f83c24
Alekslov <73888669+Alekslov@users.noreply.github.com>
2020-12-07 12:37:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,18 +4,21 @@ import { renderClock } from './components/clock/renderClock.js';
 
 import { renderAllProgressBars } from './components/progress-bar/renderAllProgressBars.js';
 import { progressBarData } from './data/progressBarData.js';
+
 import { formValidator } from './components/form-validator/formValidator.js';
+
 import { Toast } from './components/toast/Toast.js';
+
 renderSocials('footer > .row', socialsData);
 
 renderClock('.clock');
 
 renderAllProgressBars(progressBarData);
+
 const toast = new Toast();
 toast.render();
 // toast.show('error', 'Cia yra klaida!!!');
 // toast.show('success', 'Buvo gera diena!!! ;)');
 
-
-formValidator('.hero .form');
-formValidator('main .form'); 
+formValidator('.hero .form', toast);
+formValidator('main .form', toast);
