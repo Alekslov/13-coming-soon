51f16dedfbfe5e1130f94595cf911da3d635bee2
Alekslov <Alekslov@users.noreply.github.com>
2020-12-07 12:29:44 +0200

    toasting

--- js/components/form-validator/formValidator.js
+++ js/components/form-validator/formValidator.js
@@ -1,44 +1,54 @@
-import { isValidEmail, isValidName, isValidText } from './validationRules.js';
+import { validation } from './validationRules.js';
 
-function formValidator(selector) {
+/**
+ * Formos validavima atliekanti funkcija, kuri automatiskai atpazista kokiems ivesties laukams kokias reikia taikyti valydacijos taisykles ir pagal tai atvaizduoja atitinkamus pranesimus
+ * 
+ * @param {string} selector CSS like selector
+ * @param {Object} toastObject Objektas i kuri reikia kreiptis, norint atvaizduoti pranesimus: tiek sekmes, tiek klaidos.
+ * @returns {boolean} Funkcijai sekmingai suveikus grazinamas `true`, priesingu atveju `false`;
+ */
+function formValidator(selector, toastObject) {
     const formDOM = document.querySelector(selector);
     const submitBtnDOM = formDOM.querySelector('input[type="submit"]');
+
+    if (!submitBtnDOM) {
+        toastObject.show('error', 'ERROR: formoje nerasta input:submit mygtukas.');
+        return false;
+    }
 
     const allInputDOMs = formDOM.querySelectorAll('input:not([type="submit"])');
     const allTextareaDOMs = formDOM.querySelectorAll('textarea');
 
     const allElements = [...allInputDOMs, ...allTextareaDOMs];
 
-    submitBtnDOM.addEventListener('click', () => {
+    if (allElements.length === 0) {
+        toastObject.show('error', 'ERROR: formoje nerasta nei vieno input ar textarea elementu.');
+        return false;
+    }
+
+    submitBtnDOM.addEventListener('click', event => {
+        event.preventDefault();
         let errorCount = 0;
-        console.clear();
 
         for (let input of allElements) {
             const validationRule = input.dataset.validation;
             const text = input.value;
 
-            const nameError = isValidName(text);
-            const emailError = isValidEmail(text);
-            const textError = isValidText(text);
-
-            if (validationRule === 'name' && nameError !== true) {
-                console.log(nameError);
+            const validationFunction = validation[validationRule];
+            const error = validationFunction(text);
+            if (error !== true) {
+                toastObject.show('error', error);
                 errorCount++;
-            }
-            if (validationRule === 'email' && emailError !== true) {
-                console.log(emailError);
-                errorCount++;
-            }
-            if (validationRule === 'text' && textError !== true) {
-                console.log(textError);
-                errorCount++;
+                break;  // padaro, jog klaidos pranesima mestu ties pirma sutikta klaida
             }
         }
 
         if (errorCount === 0) {
-            console.log('Siumciam info...');
+            toastObject.show('success', 'Siunciam info...');
         }
     })
+
+    return true;
 }
 
 export { formValidator }
